7ac2a07ae0d262026abc1b740a83ee7891e0ea91
Abdullah khan <khanabdullah4289951@gmail.com>
2018-08-17 18:54:49 +0500

    Update server.js

--- server.js
+++ server.js
@@ -121,12 +121,13 @@ app.post('/deleteItem', function (req, res) {
 
 
 */
-/*const con = mysql.createConnection({
-    host: 'creesol.cz7pwzetgifa.ap-south-1.rds.amazonaws.com',
-    user: 'creesol_admin',
-    password: 'Kashif123',
-    database: 'mydb'
-});*/
+const con = mysql.createConnection({
+    host: 'knockdatabase.cz7pwzetgifa.ap-south-1.rds.amazonaws.com',
+    user: 'creesol',
+    password: 'creesol.com',
+    database: 'mydb',
+    port: '3306'
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
