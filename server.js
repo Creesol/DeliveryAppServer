7e4ae12dbddcebdb52c27e2b8692bc7682dc5a97
Abdullah khan <khanabdullah4289951@gmail.com>
2018-08-17 20:20:54 +0500

    Update server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.post('/deleteItem', function (req, res) {
 
 */
 const con = mysql.createConnection({
-    host: '13.232.147.28',
+    host: 'knockdatabase.cz7pwzetgifa.ap-south-1.rds.amazonaws.com',
     user: 'creesol',
     password: 'creesol.com',
     database: 'mydb',
