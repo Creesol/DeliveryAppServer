6a2a352eb244c35fc89a9ce736e53ae87814087e
Abdullah khan <khanabdullah4289951@gmail.com>
2018-08-18 17:32:12 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const app= express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const url = require('url');
 const querystring = require('querystring'); 
