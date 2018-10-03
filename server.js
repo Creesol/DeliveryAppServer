e8096e33d7dbd6deba048caa8a017dc6d98aca4f
kashif zahid <kashif.zahid18@gmail.com>
2018-10-03 14:05:51 +0500

    Update server.js

--- server.js
+++ server.js
@@ -898,7 +898,7 @@ app.get('/CategorySpinner', function (req, res) {
 });
 
 
-app.get('/UpdatePrice', function (req, res) {
+app.post('/UpdatePrice', function (req, res) {
     var query="UPDATE `mydb`.`category_detail` SET `updated_price`="+mysql.escape(req.body.rate)+"WHERE `item_id`="+mysql.escape(req.body.itemId);     
     con.getConnection(function (err, connection) {
         if (err) {
@@ -919,7 +919,7 @@ app.get('/UpdatePrice', function (req, res) {
    
 });
 
-app.get('/UpdateItemData', function (req, res) {
+app.post('/UpdateItemData', function (req, res) {
     var query="UPDATE `mydb`.`category_detail` SET `name`="+mysql.escape(req.body.name)+", `image`="+mysql.escape(req.body.image)+", `description`="+mysql.escape(req.body.desc)+", `measuring_unit`="+mysql.escape(req.body.measure)+", `updated_price`="+mysql.escape(req.body.rate)+", `sub_category_name`="+mysql.escape(req.body.subcat)+", `quantity`="+mysql.escape(req.body.quantity)+" WHERE `item_id`="+mysql.escape(req.body.itemId);
     
     con.getConnection(function (err, connection) {
@@ -965,7 +965,7 @@ app.get('/subCategorySpinner', function (req, res) {
 });
 
 
-app.get('/insertNewItem', function (req, res) {
+app.post('/insertNewItem', function (req, res) {
     var query="INSERT INTO `mydb`.`category_detail` (`name`, `image`, `description`, `measuring_unit`, `updated_price`, `sub_category_name`, `_cat_id`, `quantity`) VALUES ("+mysql.escape(req.body.name)+","+ mysql.escape(req.body.image)+","+ mysql.escape(req.body.desc)+","+mysql.escape(req.body.measure)+","+mysql.escape(req.body.rate)+","+mysql.escape(req.body.subcat)+","+mysql.escape(req.body.catId)+","+mysql.escape(req.body.quantity)+")";   
     con.getConnection(function (err, connection) {
         if (err) {
