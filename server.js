da9d47c9d453de9af4d8789dbbedbc4aa389da69
kashif zahid <kashif.zahid18@gmail.com>
2018-10-21 20:57:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -331,6 +331,10 @@ app.get('/getCartName', function (req, res) {
     var query = "SELECT distinct sub_category_name FROM mydb.category_detail where _cat_id=3";
     handle_database(query, req, res);
 });
+app.get('/getCartNameDetail', function (req, res) {
+    var query = "select * from category_detail where sub_category_name ="+mysql.escape(req.query.subcat)+" LIMIT 7";
+    handle_database(query, req, res);
+});
           
 
             app.post('/postUserData', function (req, res) {
